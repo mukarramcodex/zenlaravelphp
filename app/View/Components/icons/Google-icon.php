184b0d5be903e7956d9f2e7616ed86632da3598e
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-17 15:42:00 +0500

    Google Icon updates

--- app/View/Components/icons/Google-icon.php
+++ app/View/Components/icons/Google-icon.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Google-icon extends Component
+class Googleicon extends Component
 {
     /**
      * Create a new component instance.
