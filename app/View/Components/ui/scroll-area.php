eecbe20478aab501ed560ca069ff6c892636e50f
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-24 00:32:37 +0500

    PHP Chanings

--- app/View/Components/ui/scroll-area.php
+++ app/View/Components/ui/scroll-area.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class scroll-area extends Component
+class scrollarea extends Component
 {
     /**
      * Create a new component instance.
