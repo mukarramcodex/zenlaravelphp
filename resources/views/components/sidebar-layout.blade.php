bf3787172351825374e1b46059b1423d964b9a94
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-24 00:55:41 +0500

    SideBar

--- resources/views/components/sidebar-layout.blade.php
+++ resources/views/components/sidebar-layout.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
+<div class="flex h-screen bg-gray-200">
+    <div class="sidebar bg-blue-500 text-white w-64 min-h-screen fixed transition-all duration-300 ease-in-out">
+        @include('layouts.sidebar')
+    </div>
+    <div class="flex flex-1 flex-col overflow-hidden ml-64">
+        <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-4">
+            <div title="{{ isset($title) ? $title : '' }}" subtitle="{{ isset($subtitle) ? $subtitle : "" }}"></div>
+            {{ $slot }}
+        </main>
+    </div>
 </div>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,3 +1,25 @@
-<div>
-    <!-- We must ship. - Taylor Otwell -->
-</div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale() ) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-tokent" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'ZEN APP') }}</title>
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="sidebar bg-blue-800 text-white w-64 min-h-screen fixed transition-all duration-300 ease-in-out">
+        <div class="sidebar-header p-4 border-b border-gray-700 flex items-center">
+            <div class="logo flex items-center">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
